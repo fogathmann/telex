7dd53f3a98e1a85b159bb1f81dfabb6910f5e95b
F. Oliver Gathmann <fogathmann@gmail.com>
2014-10-08 09:50:04 +0200

    Second attempt at pointing the everest dependency to the latest master.

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(name='telex',
       package_data={'': ["*.zcml"]},
       include_package_data=True,
       zip_safe=False,
-      install_requires=['everest-dev'],
+      install_requires=['everest>1.0'],
       dependency_links=
         ['https://github.com/gathmann/telex/tarball/master#egg=telex',
-         'https://github.com/cenix/everest/tarball/master#egg=everest-dev'],
+         'https://github.com/cenix/everest/tarball/master#egg=everest'],
       entry_points="""\
       [paste.app_factory]
       app = everest.run:app_factory
